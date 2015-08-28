83b02a09d0889da7451220cf9cd275c229a0654b
Jambor Yao <jamboryao@hotmail.com>
2015-08-28 10:06:32 +0800

    init

---
+++ NodeLearning/server.js
@@ -0,0 +1,7 @@
+﻿var http = require('http');
+var port = process.env.port || 1337;
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Node.js\n');
+}).listen(port);
+console.log("listening at http://127.0.0.1:1337");
